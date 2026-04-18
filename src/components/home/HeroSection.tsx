167e8fa1f567f238ea141eae578cf99f13949e26
shoronshaha <smaranshaha@gmail.com>
2026-04-18 16:34:56 +0600

    feat: add HeroSection component with animated typography and CV download links

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -52,7 +52,7 @@ const HeroSection = () => {
 
           </Link>
           <Link
-            href="https://drive.google.com/file/d/1dxk2_D3inyL6fkZBFVvmkRGEp_rG-D4A/view?usp=sharing"
+            href="https://drive.google.com/file/d/1vzh9jsCnlc_vsUXtOun1SNj9psF3pY6i/view?usp=sharing"
             target="_blank"
           >
             <Button
